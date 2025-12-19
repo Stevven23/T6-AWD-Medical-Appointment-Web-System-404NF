cb207a4b9900e7079ddf1935d711108eb2c8b933
Michael Simbaña <michaelrivadeneira99@gmail.com>
2025-12-18 21:45:51 -0500

    Fix Server01

--- 03-Code/MedicalAppointment/backend/server.js
+++ 03-Code/MedicalAppointment/backend/server.js
@@ -4,6 +4,16 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const supabase = require('./database');
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.sendStatus(204);
+  }
+  next();
+});
 const passport = require('./config/passport');
 
 const app = express();
