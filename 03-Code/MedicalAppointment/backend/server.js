a50581476466804785c68b5958ee3966677d3314
Michael Simbaña <michaelrivadeneira99@gmail.com>
2025-12-18 21:48:18 -0500

    Fix Server02

--- 03-Code/MedicalAppointment/backend/server.js
+++ 03-Code/MedicalAppointment/backend/server.js
@@ -1,3 +1,15 @@
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -5,15 +17,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const supabase = require('./database');
 
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.sendStatus(204);
-  }
-  next();
-});
+
 const passport = require('./config/passport');
 
 const app = express();
